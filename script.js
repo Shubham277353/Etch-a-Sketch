5fbffb77131e173584c4d4826fd0cfbee004b507
Shubham277353 <golubhandari405@gmail.com>
2025-03-05 23:02:16 +0530

    added style.css and script.js

---
+++ script.js
@@ -0,0 +1,78 @@
+const container = document.querySelector(".container");
+const body = document.querySelector("body");
+const title = document.createElement("div");
+const controls = document.querySelector("#controls");
+const create = document.querySelector("#createButton");
+const reset = document.querySelector("#reset");
+
+// title styling
+title.textContent = "Etch-a-Sketch";
+title.classList.add("head-title");
+body.insertBefore(title, body.firstChild);
+
+create.addEventListener("click", () => {
+  updateGrid();
+});
+
+function squareGrid(count) {
+  isPainting = false;
+  container.innerHTML = "";
+  const size = 500 / count;
+
+  for (let i = 1; i <= count * count; i++) {
+    const square = document.createElement("div");
+    square.style.height = size + "px";
+    square.style.width = size + "px";
+    square.classList.add(`squares`);
+
+    square.addEventListener("mousedown", () => {
+      isPainting = true;
+      square.style.backgroundColor = "Black";
+    });
+
+    square.addEventListener("mouseenter", () => {
+      if (isPainting) {
+        square.style.backgroundColor = "black";
+      }
+    });
+
+    document.addEventListener("mouseup", () => {
+      isPainting = false;
+    });
+
+    reset.addEventListener("click", () => {
+      square.style.backgroundColor = "white";
+    });
+
+    let darkness = 0;
+
+    function darkenSquare() {
+        if (darkness < 10) { 
+            darkness += 1;  
+            let lightness = 100 - (darkness * 10); 
+            square.style.backgroundColor = `hsl(0, 0%, ${lightness}%)`;
+        }
+    }
+
+    square.addEventListener('mousedown', darkenSquare);
+
+    square.addEventListener('mouseenter', () => {
+        if (isPainting) {
+            darkenSquare();
+        }
+    });
+
+    container.appendChild(square); 
+  }
+}
+
+function updateGrid() {
+  const newSize = document.getElementById("gridSize").value;
+  if (newSize > 0 && newSize <= 100) {
+    squareGrid(newSize);
+  } else {
+    alert("Please enter a valid number between 1 and 100. ");
+  }
+}
+
+squareGrid(16);
