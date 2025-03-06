ec792e793fa186f0a00725e276b2b4c346de72ac
Shubham277353 <golubhandari405@gmail.com>
2025-03-06 14:22:58 +0530

    added shading and rainbow buttons which allows sh- ading and random color generation .

--- script.js
+++ script.js
@@ -4,18 +4,30 @@ const title = document.createElement("div");
 const controls = document.querySelector("#controls");
 const create = document.querySelector("#createButton");
 const reset = document.querySelector("#reset");
+const shade = document.querySelector("#shade");
+const rainbow = document.querySelector("#rainbow");
 
-// title styling
 title.textContent = "Etch-a-Sketch";
 title.classList.add("head-title");
 body.insertBefore(title, body.firstChild);
+
+let toggleShade = false;
+let toggleRainbow = false;
+
+shade.addEventListener("click", () => {
+  toggleShade = !toggleShade;
+});
+
+rainbow.addEventListener("click", () => {
+  toggleRainbow = !toggleRainbow;
+});
 
 create.addEventListener("click", () => {
   updateGrid();
 });
 
 function squareGrid(count) {
-  isPainting = false;
+  let isPainting = false;
   container.innerHTML = "";
   const size = 500 / count;
 
@@ -47,22 +59,37 @@ function squareGrid(count) {
     let darkness = 0;
 
     function darkenSquare() {
-        if (darkness < 10) { 
-            darkness += 1;  
-            let lightness = 100 - (darkness * 10); 
-            square.style.backgroundColor = `hsl(0, 0%, ${lightness}%)`;
-        }
+      if (darkness < 10) {
+        darkness += 1;
+        let lightness = 100 - darkness * 10;
+        square.style.backgroundColor = `hsl(0, 0%, ${lightness}%)`;
+      }
     }
 
-    square.addEventListener('mousedown', darkenSquare);
+    function randomColor() {
+      if (isPainting && toggleRainbow) {
+        const red = Math.floor(Math.random() * 256);
+        const green = Math.floor(Math.random() * 256);
+        const blue = Math.floor(Math.random() * 256);
+        square.style.backgroundColor = `rgb(${red},${green},${blue})`;
+      }
+    }
 
-    square.addEventListener('mouseenter', () => {
-        if (isPainting) {
-            darkenSquare();
-        }
+    square.addEventListener("mousedown", randomColor);
+    square.addEventListener("mousedown", darkenSquare);
+
+    square.addEventListener("mouseenter", () => {
+      if (isPainting && toggleShade) {
+        darkenSquare();
+      }
+    });
+    square.addEventListener("mouseenter", () => {
+      if (isPainting && toggleRainbow) {
+        randomColor();
+      }
     });
 
-    container.appendChild(square); 
+    container.appendChild(square);
   }
 }
 
